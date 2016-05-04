1f7aa254ed2f3eec3e42ecdb8d8faccfa2f261cf
qjx <183526811@qq.com>
2016-05-04 19:23:57 +0800

    新增CrossWalk样例。

--- app/src/main/java/com/jacenchiu/crosswalkdemo/MainActivity.java
+++ app/src/main/java/com/jacenchiu/crosswalkdemo/MainActivity.java
@@ -1,13 +1,49 @@
 package com.jacenchiu.crosswalkdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.xwalk.core.XWalkView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private XWalkView mXWalkWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mXWalkWebView = (XWalkView) findViewById(R.id.xwv_content);
+        mXWalkWebView.load("http://crosswalk-project.org/", null);
+
+        // turn on debugging
+        //XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mXWalkWebView != null) {
+            mXWalkWebView.pauseTimers();
+            mXWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mXWalkWebView != null) {
+            mXWalkWebView.resumeTimers();
+            mXWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mXWalkWebView != null) {
+            mXWalkWebView.onDestroy();
+        }
     }
 }
